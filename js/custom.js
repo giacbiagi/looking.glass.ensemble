14a43a29ba27e0f5cfab899c35cbc0c3c471f0ba
giac <giac.biagi@gmail.com>
2019-07-09 21:08:55 +0200

    Soccorso Enzo

--- js/custom.js
+++ js/custom.js
@@ -31,16 +31,18 @@ $(document).ready(function() {
  /* Home Slideshow Vegas
   -----------------------------------------------*/
   $(function() {
-    $('body').vegas({
-        slides: [
-            { src: 'images/slide-img1.jpg' },
-            { src: 'images/slide-img2.jpg' },
-            { src: 'images/slide-img3.jpg' }
-        ],
-        timer: false,
-        transition: [ 'zoomIn', ],
-        animation: ['kenburns']
-    });
+    if($('.member-page').length == 0) {
+      $('body').vegas({
+          slides: [
+              { src: 'images/slide-img1.jpg' },
+              { src: 'images/slide-img2.jpg' },
+              { src: 'images/slide-img3.jpg' }
+          ],
+          timer: false,
+          transition: [ 'zoomIn', ],
+          animation: ['kenburns']
+      });
+    }
   });
 
 
